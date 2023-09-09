11d4eb8ac4beba05ba5d366bfcdf2f06025bb771
1 <1@DESKTOP-IBIKR2A>
2023-09-09 11:44:08 +0500

    Add project files.

---
+++ Tyuiu.karimov.sprin0.task0.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.karimov.sprin0.task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
